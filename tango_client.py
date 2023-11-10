9e71124f06f3e56794d3bffd21ae58e4eedc0e6e
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-11-10 14:39:00 +0100

    wait until motor stops, before sending it new position
    It turns out you can set new Position to the motor, while
    it is moving.

--- tango_client.py
+++ tango_client.py
@@ -1,4 +1,17 @@
+import time
 from tango import DeviceProxy
+
+
+
+def _while_moving(*devs):
+   while True:
+     is_moving = [dev.StatusMoving for dev in devs]
+     print(is_moving)
+     if not any(is_moving):
+       print("the all stopped!")
+       return
+
+     time.sleep(0.2)
 
 
 class TangoClient:
@@ -10,5 +23,6 @@ class TangoClient:
         print(self.pol.ping())
 
     def set_angles(self, base, mount):
+        _while_moving(self.azi, self.pol)
         self.azi.Position = base
         self.pol.Position = mount
